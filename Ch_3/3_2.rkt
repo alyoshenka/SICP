093b43adff649f62be3dcd756bd905c39d2ed8ea
Alexi <alexibmost@gmail.com>
2022-10-09 10:43:42 -0700

    3.2

--- Ch_3/3_2.rkt
+++ Ch_3/3_2.rkt
@@ -1,1 +1,17 @@
 #lang racket
+
+(define (make-monitored f)
+  (let ((cnt 0))
+    (define (eval x)
+      (begin (set! cnt (+ cnt 1))
+             (f x)))
+    (define (dispatch m)
+      (cond ((eq? m 'how-many-calls?) cnt)
+            (else (eval m))))
+    dispatch))
+
+(define s (make-monitored sqrt))
+(s 100) ; 10
+(s 'how-many-calls?) ; 1
+
+                    
